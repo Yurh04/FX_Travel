461b3b4d14be0e58bce67d56d935a50ad909af00
杨树 <23373315>
2025-05-19 22:04:30 +0800

    revoke Test

--- src/test/java/org/fxtravel/fxspringboot/FxSpringbootApplicationTests.java
+++ src/test/java/org/fxtravel/fxspringboot/FxSpringbootApplicationTests.java
@@ -1,13 +1,13 @@
-package org.fxtravel.fxspringboot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FxSpringbootApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.fxtravel.fxspringboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FxSpringbootApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
